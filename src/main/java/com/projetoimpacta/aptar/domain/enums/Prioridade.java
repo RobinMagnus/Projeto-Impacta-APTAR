42d7c28940e0a85adad9f0084bcda17dadb61c6d
Mateus Silva <tech.mateussilva@gmail.com>
2024-02-23 14:55:57 -0300

    Tipos enums - Perfil, Prioridade, Status

---
+++ src/main/java/com/projetoimpacta/aptar/domain/enums/Prioridade.java
@@ -0,0 +1,42 @@
+package com.projetoimpacta.aptar.domain.enums;
+
+public enum Prioridade {
+
+    BAIXA(0, "BAIXA"),
+    MEDIA(1, "MEDIA"),
+    ALTA(2, "ALTA");
+
+
+    private final Integer codigo;
+    private final String descricao;
+
+
+    Prioridade(Integer codigo, String descricao) {
+        this.codigo = codigo;
+        this.descricao = descricao;
+    }
+
+
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+
+    public static Prioridade toEnums(Integer cod) {
+        if (cod == null) {
+            return null;
+        }
+
+        for (Prioridade x : Prioridade.values()) {
+            if (cod.equals(x.getCodigo())) {
+                return x;
+            }
+        }
+
+        throw new IllegalArgumentException("Prioridade Inválida");
+    }
+}
