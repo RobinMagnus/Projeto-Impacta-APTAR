012f36f4a28f32cc78839e1a0722c55c0df194c5
Mateus Silva <tech.mateussilva@gmail.com>
2024-02-23 14:45:39 -0300

    Projeto criado

---
+++ src/main/java/com/projetoimpacta/aptar/AptarApplication.java
@@ -0,0 +1,13 @@
+package com.projetoimpacta.aptar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AptarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AptarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetoimpacta/aptar/AptarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetoimpacta.aptar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AptarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
