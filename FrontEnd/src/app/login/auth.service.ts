c99ecb1827970a7732b80ea8c2c6772e3521b03f
RobinMagnus <robbin333@outlook.com.br>
2024-04-14 17:03:42 -0300

    esperando atualizações

--- FrontEnd/src/app/login/auth.service.ts
+++ FrontEnd/src/app/login/auth.service.ts
@@ -1,7 +1,9 @@
-import { EventEmitter, Injectable } from '@angular/core';
+import { EventEmitter, Injectable, Input } from '@angular/core';
 import { Usuario } from './usuario';
 import { Router } from '@angular/router';
 import { Form } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { Observable, forkJoin, map, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,17 +12,55 @@ export class AuthService {
   
   usuarioAutenticado:boolean = false;
 
+  @Input() tecnicoEncontrado?:any;
+  empresaEncontrada?:any;
+
   mostrarMenuEmitter = new EventEmitter<boolean>();
+  
 
+  constructor(private router: Router,
+    private http: HttpClient
+  ) { }
 
-  constructor(private router: Router) { }
+  login(email: string): Observable<any> {
+    const loginTecnico$ = this.http.get<any>('http://localhost:8080/tecnicos');
+    const loginEmpresa$ = this.http.get<any>('http://localhost:8080/empresas');
+  
+    return forkJoin([loginTecnico$, loginEmpresa$]).pipe(
+      tap(([tecnicos, empresas]) => {
+        console.log('Tecnicos:', tecnicos);
+        console.log('Empresas:', empresas);
+      }),
+      map(([tecnicos, empresas]) => {
+        console.log('Email:', email);
+  
+        this.tecnicoEncontrado = tecnicos.find((item:any) => item.email == email);
+        this.empresaEncontrada = empresas.find((item:any) => item.email == email);
+  
+        console.log('Técnico encontrado:', this.tecnicoEncontrado);
+        console.log('Empresa encontrada:', this.empresaEncontrada);
+  
+        return [this.tecnicoEncontrado, this.empresaEncontrada];
+      })
+    );
+  }
+  
+  getTecnicoEncontrado() {
+    return this.tecnicoEncontrado;
+  }
+  getEmpresaEncontrada() {
+    return this.empresaEncontrada;
+  }
+  
 
   fazerLogin(usuario:Usuario){
     if(usuario.email === 'usuario@email.com' &&
     usuario.senha === '123') {
+      const idUsuario = 2;
+      usuario.id = idUsuario;
       this.usuarioAutenticado = true;
       this.mostrarMenuEmitter.emit(true);
-      this.router.navigate(['/consultas']);
+      this.router.navigate(['/consultas', usuario.id]);
 
     }else {
       this.usuarioAutenticado = false;
--- FrontEnd/src/app/login/login.component.ts
+++ FrontEnd/src/app/login/login.component.ts
@@ -1,27 +1,53 @@
 import { Component } from '@angular/core';
-import {FormBuilder, FormGroup} from "@angular/forms";
+import {FormBuilder, FormGroup, FormsModule} from "@angular/forms";
 import {CadastroService} from "../services/cadastro.service";
 import { AuthService } from './auth.service';
 import { Usuario } from './usuario';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
 
-  public usuario: Usuario = {email:'', senha:''};
-  
-  constructor(private authService: AuthService
-  ) {
+ 
+  public email: string ='';
+  public senha: string = '';
+
+  constructor(private authService: AuthService,
+    private router: Router) {
     
   }
 
-  onSubmit(){
-    console.log(this.usuario);
-    this.authService.fazerLogin(this.usuario);
+  login() {
+    this.authService.login(this.email).subscribe(
+      (res) => {
+        console.log(res)
+        let isCPF = false;
+        let isCNPJ = false;
+  
+        if (res[0] && res[0].cpf) {
+          isCPF = true;
+        }
+  
+        if (res[1] && res[1].cnpj) {
+          isCNPJ = true;
+        }
+  
+        if (isCPF) {
+          this.router.navigate(['consultas', 'consultaTecnico'], { state: { tipoUsuario: res[0].cpf } });
+        } else if (isCNPJ) {  
+          this.router.navigate(['consultas', 'consultaEmpresa'], { state: { tipoUsuario: res[1].cnpj } });
+        } else {
+        }
+      },
+      (error) => {
+        console.error('Erro ao fazer login:', error);
+      }
+    );
   }
-
 
 }
