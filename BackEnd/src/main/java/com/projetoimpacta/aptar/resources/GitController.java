a263da26e60f347420885cb02e8a62f7ee1a4e8b
Mateus Silva <tech.mateussilva@gmail.com>
2024-05-05 18:38:46 -0300

    Upload concluído

--- BackEnd/src/main/java/com/projetoimpacta/aptar/resources/GitController.java
+++ BackEnd/src/main/java/com/projetoimpacta/aptar/resources/GitController.java
@@ -21,9 +21,6 @@ import java.io.IOException;
 @RequestMapping("/git")
 public class GitController {
 
-    @Autowired
-    private FormsFinalizacaoService formsFinalizacaoService;
-
     private static final String REPO_URL = "https://github.com/CodeByTeusSilva/Projeto-Impacta-APTAR.git";
     private static final String LOCAL_DIR = "C:\\Workspace Java\\aptar - (Teste upload)\\Projeto-Impacta-APTAR-main";
     private static final String COMMIT_MESSAGE = "Add image";
@@ -35,65 +32,16 @@ public class GitController {
     private String TOKEN;
 
     @PostMapping("/upload")
-    public String uploadImage(@RequestParam("chamadoId") Long chamadoId,
-                              @RequestParam("observacoes") String observacoes,
-                              @RequestParam("file") MultipartFile file) {
+    public String uploadImage(@RequestParam("file") MultipartFile file) {
         String result = "";
         File uploadFile = new File(UPLOAD_DIR + "/" + file.getOriginalFilename());
 
         try {
-            // Verifica se a pasta de upload existe, caso contrário, cria a pasta
-            File uploadDir = new File(UPLOAD_DIR);
-            if (!uploadDir.exists()) {
-                boolean dirCreated = uploadDir.mkdirs();
-                if (!dirCreated) {
-                    return "Erro ao criar a pasta de upload: " + UPLOAD_DIR;
-                }
-            }
-
             // Salvar o arquivo na pasta de upload
-            String originalFilename = file.getOriginalFilename();
-            uploadFile = new File(UPLOAD_DIR, originalFilename);
             file.transferTo(uploadFile);
 
-            // Cria uma instância de FormsFinalizacaoDTO
-            FormsFinalizacaoDTO formsFinalizacaoDTO = new FormsFinalizacaoDTO();
-            formsFinalizacaoDTO.setObservacoes(observacoes);
-            formsFinalizacaoDTO.setFotoUrl(originalFilename);
-
-            // Chamar o serviço para criar ou atualizar FormsFinalizacao
-            formsFinalizacaoService.create(chamadoId, formsFinalizacaoDTO, file);
-
-            // Clonar ou abrir o repositório localmente
-            Git git = null;
-            File gitDirectory = new File(LOCAL_DIR);
-
-            if (gitDirectory.exists() && gitDirectory.isDirectory()) {
-                git = Git.open(gitDirectory);
-            } else {
-                git = Git.cloneRepository()
-                        .setURI(REPO_URL)
-                        .setDirectory(gitDirectory)
-                        .call();
-            }
-
-            // Adicionar o arquivo à pasta de upload
-            git.add().addFilepattern("upload/" + file.getOriginalFilename()).call();
-
-            // Cometer a alteração
-            git.commit()
-                    .setMessage(COMMIT_MESSAGE)
-                    .setAuthor(new PersonIdent("Your Name", "your-email@example.com"))
-                    .call();
-
-            // Enviar as alterações para o repositório remoto
-            git.push()
-                    .setCredentialsProvider(new UsernamePasswordCredentialsProvider(USERNAME, TOKEN))
-                    .setRemote("origin")
-                    .call();
-
             result = "Imagem enviada com sucesso!";
-        } catch (IOException | GitAPIException e) {
+        } catch (IOException e) {
             e.printStackTrace();
             result = "Erro ao enviar a imagem: " + e.getMessage();
         }
