a3983f48b91b5837d7b606ddc8612b9b2cc734e7
Mateus Silva <tech.mateussilva@gmail.com>
2024-05-05 18:46:48 -0300

    Upload concluído

--- BackEnd/src/main/java/com/projetoimpacta/aptar/services/FormsFinalizacaoService.java
+++ BackEnd/src/main/java/com/projetoimpacta/aptar/services/FormsFinalizacaoService.java
@@ -48,7 +48,7 @@ public class FormsFinalizacaoService {
 
     private String saveFile(MultipartFile file) throws IOException {
         // Diretório onde o arquivo será salvo
-        String uploadDir = "C:\\Workspace Java\\aptar - (Teste upload)\\Projeto-Impacta-APTAR-main\\upload";
+        String uploadDir = "caminho/do/diretorio/upload";
         // Nome do arquivo original enviado
         String originalFilename = file.getOriginalFilename();
         // Crie um novo arquivo no diretório de upload
