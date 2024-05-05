14d5d525d3960cafa900ed86f975af35c59c24e0
Mateus Silva <tech.mateussilva@gmail.com>
2024-05-05 18:57:24 -0300

    Upload concluído

--- BackEnd/src/main/java/com/projetoimpacta/aptar/resources/GitController.java
+++ BackEnd/src/main/java/com/projetoimpacta/aptar/resources/GitController.java
@@ -40,37 +40,8 @@ public class GitController {
             // Salvar o arquivo na pasta de upload
             file.transferTo(uploadFile);
 
-            // Clonar ou abrir o repositório localmente
-            Git git = null;
-            File gitDirectory = new File(LOCAL_DIR);
-
-            if (gitDirectory.exists() && gitDirectory.isDirectory()) {
-                git = Git.open(gitDirectory);
-            } else {
-                git = Git.cloneRepository()
-                        .setURI(REPO_URL)
-                        .setDirectory(gitDirectory)
-                        .call();
-            }
-
-            // Adicionar o arquivo à pasta de upload
-            git.add().addFilepattern("upload/" + file.getOriginalFilename()).call();
-
-
-            // Cometer a alteração
-            git.commit()
-                    .setMessage(COMMIT_MESSAGE)
-                    .setAuthor(new PersonIdent("Your Name", "your-email@example.com"))
-                    .call();
-
-            // Enviar as alterações para o repositório remoto
-            git.push()
-                    .setCredentialsProvider(new UsernamePasswordCredentialsProvider(USERNAME, TOKEN))
-                    .setRemote("origin")
-                    .call();
-
             result = "Imagem enviada com sucesso!";
-        } catch (IOException | GitAPIException e) {
+        } catch (IOException e) {
             e.printStackTrace();
             result = "Erro ao enviar a imagem: " + e.getMessage();
         }
