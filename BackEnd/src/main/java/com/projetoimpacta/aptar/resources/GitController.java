492567207b4ed99bc225f8665c63d3c985b6076f
Mateus Silva <tech.mateussilva@gmail.com>
2024-05-05 12:00:36 -0300

    Upload concluído

--- BackEnd/src/main/java/com/projetoimpacta/aptar/resources/GitController.java
+++ BackEnd/src/main/java/com/projetoimpacta/aptar/resources/GitController.java
@@ -21,6 +21,9 @@ import java.io.IOException;
 @RequestMapping("/git")
 public class GitController {
 
+    @Autowired
+    private FormsFinalizacaoService formsFinalizacaoService;
+
     private static final String REPO_URL = "https://github.com/CodeByTeusSilva/Projeto-Impacta-APTAR.git";
     private static final String LOCAL_DIR = "C:\\Workspace Java\\aptar - (Teste upload)\\Projeto-Impacta-APTAR-main";
     private static final String COMMIT_MESSAGE = "Add image";
@@ -32,13 +35,31 @@ public class GitController {
     private String TOKEN;
 
     @PostMapping("/upload")
-    public String uploadImage(@RequestParam("file") MultipartFile file) {
+    public String uploadImage(@RequestParam("chamadoId") Long chamadoId,
+                              @RequestParam("observacoes") String observacoes,
+                              @RequestParam("file") MultipartFile file) {
         String result = "";
         File uploadFile = new File(UPLOAD_DIR + "/" + file.getOriginalFilename());
 
+        File uploadDir = new File(UPLOAD_DIR);
+        if (!uploadDir.exists()) {
+            boolean dirCreated = uploadDir.mkdirs();
+            if (!dirCreated) {
+                return "Erro ao criar a pasta de upload: " + UPLOAD_DIR;
+            }
+        }
+
         try {
             // Salvar o arquivo na pasta de upload
+            String originalFilename = file.getOriginalFilename();
+            uploadFile = new File(UPLOAD_DIR, originalFilename);
             file.transferTo(uploadFile);
+
+            FormsFinalizacaoDTO formsFinalizacaoDTO = new FormsFinalizacaoDTO();
+            formsFinalizacaoDTO.setObservacoes(observacoes);
+            formsFinalizacaoDTO.setFotoUrl(originalFilename);
+
+            formsFinalizacaoService.create(chamadoId, formsFinalizacaoDTO, file);
 
             // Clonar ou abrir o repositório localmente
             Git git = null;
@@ -55,19 +76,6 @@ public class GitController {
 
             // Adicionar o arquivo à pasta de upload
             git.add().addFilepattern("upload/" + file.getOriginalFilename()).call();
-
-
-            // Cometer a alteração
-            git.commit()
-                    .setMessage(COMMIT_MESSAGE)
-                    .setAuthor(new PersonIdent("Your Name", "your-email@example.com"))
-                    .call();
-
-            // Enviar as alterações para o repositório remoto
-            git.push()
-                    .setCredentialsProvider(new UsernamePasswordCredentialsProvider(USERNAME, TOKEN))
-                    .setRemote("origin")
-                    .call();
 
             result = "Imagem enviada com sucesso!";
         } catch (IOException | GitAPIException e) {
