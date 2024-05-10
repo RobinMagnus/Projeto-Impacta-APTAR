26cd6a9a4bc0e0bca0050d77ac7338cf6f353573
Mateus Silva <tech.mateussilva@gmail.com>
2024-05-10 15:51:15 -0300

    Upload concluído

--- BackEnd/src/main/java/com/projetoimpacta/aptar/resources/GitController.java
+++ BackEnd/src/main/java/com/projetoimpacta/aptar/resources/GitController.java
@@ -5,6 +5,8 @@ import com.projetoimpacta.aptar.dtos.FormsFinalizacaoDTO;
 import com.projetoimpacta.aptar.repositories.FormsFinalizacaoRepository;
 import com.projetoimpacta.aptar.util.UploadUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -19,19 +21,23 @@ public class GitController {
     private FormsFinalizacaoRepository formsRepository;
 
     @PostMapping("/form-finalizacao")
-    public ModelAndView formFinalizacao(@ModelAttribute FormsFinalizacao formsFinalizacao, @RequestParam("File") MultipartFile imagem) {
-        ModelAndView view = new ModelAndView("form-finalizacao");
-        view.addObject("form", formsFinalizacao);
-
+    public ResponseEntity<?> formFinalizacao(@ModelAttribute FormsFinalizacao formsFinalizacaoDTO, @RequestParam("File") MultipartFile imagem) {
         try {
-            if(UploadUtil.fazerUploadImagem(imagem)){
-                formsFinalizacao.setFotoUrl(imagem.getOriginalFilename());
+            // Manipulação do upload de imagem
+            if (UploadUtil.fazerUploadImagem(imagem)) {
+                formsFinalizacaoDTO.setFotoUrl(imagem.getOriginalFilename());
             }
-            formsRepository.save(formsFinalizacao);
 
+            // Salva formsFinalizacaoDTO
+            formsRepository.save(formsFinalizacaoDTO);
+
+            // Retorna resposta com status OK
+            return ResponseEntity.ok().body(formsFinalizacaoDTO);
         } catch (Exception e) {
-            System.out.println("Salvo com sucesso: " + formsFinalizacao.getFotoUrl());
+            // Trata a exceção e retorna um status de erro
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Erro ao processar o formulário de finalização: " + e.getMessage());
         }
-        return view;
     }
 }
